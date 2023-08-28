e01064aab22868118be26c2e3ccdb584533a0220
Sergei Marichev <prophet314@gmail.com>
2023-08-28 14:33:24 +0500

    changes 28.08

--- src/cli.js
+++ src/cli.js
@@ -5,7 +5,10 @@ const userName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
+  // Я так и не понял, почему здесь функция ничего не должна возвращать...
+  // Ведь если она используется только в brain-games, в index.js нужно прописать идентичную функцию,
+  // которая передает имя пользователя в общую логику, разве нет?
+  // Почему тогда не проще просто импортировать из cli.js?
 };
 
 export default userName;
-// comment
