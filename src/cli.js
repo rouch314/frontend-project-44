f283fe09e26d5f3ecf5da798fe5706e3bd55567f
Sergei Marichev <prophet314@gmail.com>
2023-08-29 14:57:01 +0500

    changes 29.08.23

--- src/cli.js
+++ src/cli.js
@@ -4,11 +4,6 @@ const userName = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
-  // Я так и не понял, почему здесь функция ничего не должна возвращать...
-  // Ведь если она используется только в brain-games, в index.js нужно прописать идентичную функцию,
-  // которая передает имя пользователя в общую логику, разве нет?
-  // Почему тогда не проще просто импортировать из cli.js?
 };
 
 export default userName;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import readlineSync from 'readline-sync';
-import userName from './cli.js';
+
+const userName = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
 
 const gameLogic = (description, generateRoundData) => {
   const name = userName();
